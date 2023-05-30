81a6e6c9931c3f54caeec841f9e3f401d605c57e
Artkrokh <artkrokh@gmail.com>
2023-05-30 12:54:57 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 // console.log(galleryItems);
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
 function createMarkup(arr) {
-    const markup = arr.map(({ description, original, preview }) => `
+  const markup = arr
+    .map(
+      ({ description, original, preview }) => `
     <li class="gallery__item">
         <a class="gallery__link" href= "${original}" >
          <img
@@ -15,38 +17,42 @@ function createMarkup(arr) {
              alt="${description}"
          />
          </a >
-    </li > `).join('');
-     return markup
+    </li > `
+    )
+    .join("");
+  return markup;
 }
- gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems))
- gallery.addEventListener('click', handlerClick);
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+gallery.addEventListener("click", handlerClick);
 
 function handlerClick(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    if (evt.target === evt.currentTarget) {
-        return;
-    }
-    const instance = basicLightbox.create(`
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
     <div><img src="${evt.target.dataset.source}" alt="${evt.target.alt}"></div>
-`,{
-        handlerEscape: null,
-        onShow(instance) {
-            this.handlerEscape = handlerEsc.bind(instance)
-            document.addEventListener('keydown', this.handlerEscape)
-        },
-        onClose() {
-            document.removeEventListener('keydown', this.handlerEscape)
-        }
-
-    });
-
-    instance.show()
-}
- 
-function handlerEsc(evt) {
-    if (evt.code === 'Escape') {
-        console.log ('Escape');
-        this.close()
+`,
+    {
+      handlerEscape: null,
+      onShow(instance) {
+        this.handlerEscape = handlerEsc.bind(instance);
+        document.addEventListener("keydown", this.handlerEscape);
+      },
+      onClose() {
+        document.removeEventListener("keydown", this.handlerEscape);
+      },
     }
+  );
+
+  instance.show();
+}
+
+function handlerEsc(evt) {
+  if (evt.code === "Escape") {
+    console.log("Escape");
+    this.close();
+  }
 }
