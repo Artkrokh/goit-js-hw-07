8b81f13d729260653d43301c5645789b3b1a796b
Artkrokh <artkrokh@gmail.com>
2023-05-30 15:32:26 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -39,9 +39,12 @@ function handlerClick(evt) {
       handlerEscape: null,
       onShow(instance) {
         this.handlerEscape = handlerEsc.bind(instance);
+        document.addEventListener("click", this.handlerEscape);
         document.addEventListener("keydown", this.handlerEscape);
+        
       },
       onClose() {
+        document.removeEventListener("click", this.handlerEscape);
         document.removeEventListener("keydown", this.handlerEscape);
       },
     }
