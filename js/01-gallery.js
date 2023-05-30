fdb7925e793159665e3648c527ea8baced5279a4
Artkrokh <120386146+Artkrokh@users.noreply.github.com>
2023-05-30 08:54:33 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+    const markup = arr.map(({ description, original, preview }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href= "${original}" >
+         <img
+             class="gallery__image"
+             src="${preview}"
+                data-source="${original}"
+             alt="${description}"
+         />
+         </a >
+    </li > `).join('');
+     return markup
+}
